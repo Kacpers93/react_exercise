5410d14c8411fc7eab0af2743cc424a2ef24dfe5
Kacper Wołoszyn <budzio161@gmail.com>
2018-05-10 19:19:00 +0200

    trying add color class name to only one element

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,23 +4,66 @@ import './App.css';
 
 class App extends Component {
 
-constructor(props){
-  super(props);
-  this.state = {
-    addToDoes: ['Mati do zrobienia', 'Lukasz do niezrobienia', 'Rekin do wpie_dolu'],
-    message: ''
+
+  // radio button
+
+
+  // getInitialState() {
+  //     return {
+  //       selectedOption: 'option1'
+  //     };
+  //   }
+
+  // handleOptionChange(changeEvent) {
+  //   this.setState({
+  //     selectedOption: changeEvent.target.value
+  //   });
+  // }
+
+  // addToDo(e) {
+  //   formSubmitEvent.preventDefault();
+  
+  //   console.log('You have selected:', this.state.selectedOption);
+  // }
+
+  //   render: function () {
+  //     return (
+  //       <div>
+  //         somthing
+  //       </div>
+  //     );
+  //   }
+  // });
+  
+
+
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      addToDoes: ['Mati do zrobienia', 'Lukasz do niezrobienia', 'Rekin do wpie_dolu'],
+      message: ''
+    }
   }
-}
 
 
-  addToDo(e){
+  addToDo(e) {
+
     e.preventDefault();
-    const {addToDoes} = this.state;
+    const { addToDoes } = this.state;
     const newToDo = this.newToDo.value;
     const isOnTheList = addToDoes.includes(newToDo);
 
+    // 
+    // 
+    // 
+    console.log('You have selected:', this.state.selectedOption);
+    // 
+    // 
+    // 
 
-    if(isOnTheList) {
+
+    if (isOnTheList) {
       this.setState({
         message: 'You already have this task on the list.'
       })
@@ -35,7 +78,19 @@ constructor(props){
     this.addForm.reset();
   }
 
-  removeItem(item){
+  getInitialState() {
+    return {
+      selectedOption: 'option1'
+    };
+  }
+
+handleOptionChange = (changeEvent) => {
+  this.setState({
+    selectedOption: changeEvent.target.value
+  });
+}
+
+  removeItem(item) {
     const newaddToDoes = this.state.addToDoes.filter(addItem => {
       return addItem !== item;
     })
@@ -48,45 +103,61 @@ constructor(props){
 
 
   render() {
-    const {addToDoes, message} = this.state;
+    const { addToDoes, message } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <div>
-          <div className="top-text">
-            <h3>React Todo List</h3>
-          </div>
-          <div calss="app-list">
-            <div className="app-list__elemnt">
-              <div className="app-list__elemnt__text">
+        <div className="whole-box">
+          <div>
+            <div className="top-text">
+              <h3>React Todo List</h3>
+            </div>
+            <div calss="app-list">
 
-                {addToDoes.map(item => {
-                  return (
-                  <div key={item}>
-                    <p>{item}</p>
-                    <button onClick={(e) => this.removeItem(item)} type="button" className="btn-primapry">
-                      x
-                    </button>
-                  </div>)
-                })
-                }
+              {addToDoes.map(item => {
+                return (
+                  <div key={item} className={this.state.selectedOption}
+                   /*   "app-list__element"   --tu ma byc jeszcze ta klasa
+                  jak zrobić zeby przy wyborze koloru byl on zmieniany tylko po akceptacji, a nie w kazdym momencie jego zmiany/kliknięcia (tak sie aktualnie dzieje, klasa zmienia sie w kazdym elemencie)
 
-              </div>
+                  
+                  */>
+                    <div className="app-list__element__button">
+                      <button onClick={(e) => this.removeItem(item)} type="button" className="btn-remove">
+                        x
+                      </button>
+                    </div>
+                    <div className="app-list__element__text">
+                      <p>{item}</p>
+                    </div>
+                  </div>
+                )
+              })
+              }
+
             </div>
           </div>
+          <form ref={input => this.addForm = input} className="form" onSubmit={(e) => { this.addToDo(e) }}>
+            <div className="form-group">
+              <input ref={input => this.newToDo = input} type="text" placeholder="write something" className="form-control" id="newToDoInput" />
+            </div>
+            <div className="selectColor">
+              <input type="radio" className="selectColor__element orange" name="muhRadio" value="option1" checked={this.state.selectedOption === 'option1'} onChange={this.handleOptionChange} ></input>
+              <input type="radio" className="selectColor__element pink" name="muhRadio" value="option2" checked={this.state.selectedOption === 'option2'} onChange={this.handleOptionChange} ></input>
+              <input type="radio" className="selectColor__element purple" name="muhRadio" value="option3" checked={this.state.selectedOption === 'option3'} onChange={this.handleOptionChange} ></input>
+              <input type="radio" className="selectColor__element blue" name="muhRadio" value="option4" checked={this.state.selectedOption === 'option4'} onChange={this.handleOptionChange} ></input>
+              <input type="radio" className="selectColor__element green" name="muhRadio" value="option5" checked={this.state.selectedOption === 'option5'} onChange={this.handleOptionChange} ></input>
+              <input type="radio" className="selectColor__element yellow" name="muhRadio" value="option6" checked={this.state.selectedOption === 'option6'} onChange={this.handleOptionChange} ></input>
+            </div>
+            <button type="submit" className="btn-primary">Add</button>
+          </form>
+          {
+            message !== '' && <p className='message'>{message}</p>
+          }
         </div>
-        <form ref={input => this.addForm = input} className="form" onSubmit={(e) => {this.addToDo(e)}}>
-         <div className="form-group">
-          <input ref={input => this.newToDo = input} type="text" placeholder="write something" className="form-control" id="newToDoInput" />
-        </div>
-        <button  type="submit" className="btn-primary">Add</button>
-        </form>
-        {
-          message !== '' && <p className='message'>{message}</p>
-        }
       </div>
     );
   }
